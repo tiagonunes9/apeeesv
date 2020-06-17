b2b4a0a91b8df07932a3a51c770bcbf1ce976bf1
tiagonunes9 <57711248+tiagonunes9@users.noreply.github.com>
2020-06-17 19:32:38 +0100

    geral

---
+++ home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <title>Filmes</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="imagem/png" href="logo.png" />
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark bg-dark navbar">
+        <a class="navbar-brand" href="#"><img src="img/logo.png" class="logo"></a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link link" href="#">QUEM SOMOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link" href="#">EVENTOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link" href="#">NOTICIAS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link" href="#">CONTATOS</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <img src="img/escolaviriato.jpg" class="fundo" />
+    <div id="somos" clas="somos">
+        <h1> QUEM SOMOS </h1>
+    </div>
+
+    <div id="eventos" class="eventos">
+        <h1> EVENTOS </h1>
+    </div>
+
+    <div id="noticias" class="noticias">
+        <h1> NOTICIAS </h1>
+    </div>
+
+    <div id="contatos" class="contatos">
+        <h1> CONTATOS </h1>
+    </div>
+
+</body>
+
+</html>
