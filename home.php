151593defbc78348b62c04c687ae3d0c507a4165
tiagonunes9 <57711248+tiagonunes9@users.noreply.github.com>
2020-06-22 12:32:35 +0100

    Contactos && Quem Somos

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <html lang="pt">
 
 <head>
-    <title>Filmes</title>
+    <title>APEEESV</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
@@ -11,6 +11,7 @@
 
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="icon" type="imagem/png" href="logo.png" />
+    <link rel="shortcut icon" href="./simples.ico" type="image/x-icon" />
 
 </head>
 
@@ -42,38 +43,167 @@
 
     <section id="somos" class="somos">
         <div class="div-title">
-            <div class="title-divider-left"><hr></div>
-            <div class="title-h1"><h1>Quem somos</h1></div>
-            <div class="title-divider-right"><hr></div>
+            <div class="title-divider-left">
+                <hr>
+            </div>
+            <div class="title-h1">
+                <h1>Quem somos</h1>
+            </div>
+            <div class="title-divider-right">
+                <hr>
+            </div>
         </div>
-        <div class="div-content"></div>
+        <div class="div-content">
+            <p class="about">A Escola ocupa um lugar primordial na educação das crianças enquanto parte integrante de um grupo de diversos intervenientes: alunos, famílias, professores, técnicos-pedagógicos, assistentes operacionais... todos e cada um com missões insubstituíveis ao longo de todo o processo de aprendizagem.
+                Citando o Projeto Educativo da Escola Secundária Viriato 2016-2020, “Estes intervenientes não são meros atores no processo educativo: identificam-no, constroem-no e desenvolvem-no, sendo as suas atuação e interação condições básicas para o sucesso dos resultados e para a missão coletiva de desenvolvimento e valorização da sociedade em que estão integrados”. Neste sentido, entendemos que é dever da Associação de Pais e Encarregados de Educação da Escola Secundária Viriato: contribuir para o desenvolvimento saudável dos alunos através da colaboração entre os diversos intervenientes da comunidade educativa, contribuindo para o bom funcionamento da Escola e estimular a comunicação e maior participação dos pais e encarregados de educação, enquanto membros de pleno direito da comunidade escolar, promovendo uma parceria franca e cooperante entre a Escola e as famílias.
+            </p><br>
+            <h2>Órgãos Sociais da Associação de Pais e Encarregados de Educação do ano letivo de 2019/2020</2><br><br><br>
+                <img src="img/associacao.jpg" class="associacao" /><br><br>
+
+                <h3>Mesa da Assembleia Geral</h3>
+                <table class="table table-sm tabela">
+                    <tbody>
+                        <tr>
+                            <th scope="row" class="cargo">Presidente</th>
+                            <td class="nomes">Maria Helena Lopes Serra Baptista</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">Vice-Presidente</th>
+                            <td class="nomes">Maria do Rosário Silva Jesus Coelho </td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">Secretária</th>
+                            <td class="nomes">Edimeia Gonçalves </td>
+                        </tr>
+                        <td></td>
+                    </tbody>
+                </table><br>
+                <h3>Conselho Executivo</h3>
+                <table class="table table-sm tabela">
+                    <tbody>
+                        <tr>
+                            <th scope="row" class="cargo">Presidente</th>
+                            <td class="nomes">Lina Maria de Almeida Rodrigues</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">Vice-Presidente</th>
+                            <td class="nomes">Fernando de Oliveira Monteiro</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">Secretária</th>
+                            <td class="nomes">Luísa Cruz Lopes</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">Tesoureiro</th>
+                            <td class="nomes">Sandra Moreira Sebastião Monteiro</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">1º Vogal</th>
+                            <td class="nomes">Patrícia Nunes</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">2º Vogal</th>
+                            <td class="nomes">Carla Amaral</td>
+                        </tr>
+                        <td></td>
+                    </tbody>
+                </table><br>
+                <h3>Conselho Fiscal</h3>
+                <table class="table table-sm tabela">
+                    <tbody>
+                        <tr>
+                            <th scope="row" class="cargo">Presidente</th>
+                            <td class="nomes">Isabel Marisa Ferreira Pereira</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">1º Vogal</th>
+                            <td class="nomes">Domitila Martins Lima</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="cargo">2º Vogal</th>
+                            <td class="nomes">Maria do Carmo Boloto</td>
+                        </tr>
+                        <td></td>
+                    </tbody>
+                </table>
+        </div>
     </section>
 
     <section id="eventos" class="eventos">
         <div class="div-title">
-            <div class="title-divider-left"><hr></div>
-            <div class="title-h1"><h1>Eventos</h1></div>
-            <div class="title-divider-right"><hr></div>
+            <div class="title-divider-left">
+                <hr>
+            </div>
+            <div class="title-h1">
+                <h1>Eventos</h1>
+            </div>
+            <div class="title-divider-right">
+                <hr>
+            </div>
         </div>
         <div class="div-content"></div>
     </section>
 
     <section id="noticias" class="noticias">
         <div class="div-title">
-            <div class="title-divider-left"><hr></div>
-            <div class="title-h1"><h1>Notícias</h1></div>
-            <div class="title-divider-right"><hr></div>
+            <div class="title-divider-left">
+                <hr>
+            </div>
+            <div class="title-h1">
+                <h1>Notícias</h1>
+            </div>
+            <div class="title-divider-right">
+                <hr>
+            </div>
         </div>
         <div class="div-content"></div>
     </section>
 
     <section id="contatos" class="contatos">
         <div class="div-title">
-            <div class="title-divider-left"><hr></div>
-            <div class="title-h1"><h1>Contactos</h1></div>
-            <div class="title-divider-right"><hr></div>
+            <div class="title-divider-left">
+                <hr>
+            </div>
+            <div class="title-h1">
+                <h1>Contactos</h1>
+            </div>
+            <div class="title-divider-right">
+                <hr>
+            </div>
         </div>
-        <div class="div-content"></div>
+        <div class="div-content">
+            <ul class="flex-container">
+                <ul class="flex-container formulario">
+                    <h2>QUER FALAR CONNOSCO? </h2>
+                    <div class="form-group">
+                        <p class="forms">Nome</p>
+                        <input type="text" name="nome" class="form-control">
+
+                        <p class="forms">Email</p>
+                        <input type="text" name="email" class="form-control">
+
+                        <p class="forms">Assunto</p>
+                        <input type="text" name="assunto" class="form-control">
+
+                        <p class="forms">Mensagem</p>
+                        <textarea class="form-control" rows="5" cols="50"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-default enviar">ENVIAR</button>
+                </ul>
+
+                <li class="flex-x">
+                <li class="flex-item direito">
+                    <img src="img/home.png" class="icone">
+                    <p class="info">Estrada Velha de Abraveses, 3510-204 Viseu</p>
+                    <img src="img/mail.png" class="icone">
+                    <p class="info">apeeviriato@esviriato.pt</p>
+                    <img src="img/phone.png" class="icone">
+                    <p class="info">232 418 330</p><br>
+                    <p class="info">961 970 673</p>
+                </li>
+                </li>
+            </ul>
+        </div>
     </section>
 
 </body>
